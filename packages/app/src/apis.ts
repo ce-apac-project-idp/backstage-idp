31d29fa7f38c022e6d6186cd71a40b601f5ca356
Songhui Ryu <songhui.ryu@ibm.com>
2022-12-06 12:40:10 +0900

    ibm verify oidc added

--- packages/app/src/apis.ts
+++ packages/app/src/apis.ts
@@ -7,8 +7,30 @@ import {
   AnyApiFactory,
   configApiRef,
   createApiFactory,
+  ApiRef,
+  createApiRef,
+  OpenIdConnectApi,
+  ProfileInfoApi,
+  BackstageIdentityApi,
+  SessionApi,
+  discoveryApiRef,
+  oauthRequestApiRef,
 } from '@backstage/core-plugin-api';
+import { OAuth2 } from '@backstage/core-app-api'
 
+/**
+ * TODO: move this to plugin
+ */
+export const ibmOIDCAuthApiRef: ApiRef<
+  OpenIdConnectApi &      // to handle authentication
+  ProfileInfoApi &        // to request user profile to IBM Verify
+  BackstageIdentityApi &  // to associate the user profile with backstage identity
+  SessionApi              // to handle session
+  > = createApiRef({
+  id: 'auth.ibm-verify-oidc-provider',
+});
+
+// @ts-ignore
 export const apis: AnyApiFactory[] = [
   createApiFactory({
     api: scmIntegrationsApiRef,
@@ -16,4 +38,33 @@ export const apis: AnyApiFactory[] = [
     factory: ({ configApi }) => ScmIntegrationsApi.fromConfig(configApi),
   }),
   ScmAuth.createDefaultApiFactory(),
+
+  /**
+   * TODO: move this to plugin
+   */
+  createApiFactory({
+    api: ibmOIDCAuthApiRef,
+    deps: {
+      discoveryApi: discoveryApiRef,
+      oauthRequestApi: oauthRequestApiRef,
+      configApi: configApiRef,
+    },
+    factory: ({discoveryApi, oauthRequestApi, configApi}) =>
+      OAuth2.create({
+        discoveryApi,
+        oauthRequestApi,
+        provider: {
+          id: 'ibm-verify-oidc-provider',
+          title: 'IBM Security Verify as authentication provider',
+          icon: () => null,
+        },
+        environment: configApi.getOptionalString('auth.environment'),
+        defaultScopes: [
+          'openid',
+          'profile',
+          'email',
+          'phone'
+        ]
+      })
+  })
 ];
